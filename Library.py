bf505e92335d766955ddc9bca00beafdfcff8139
Michae7o <michalkotlowski7@gmail.com>
2022-10-18 18:37:54 +0200

    1. Added 'while' to automated library. 2. Updating 'add_new_book' -> can add couple books one by one. 3. Updating 'register_user' -> user_name can't be the same as other one in database. user_password now must be input when new user try register in library, password can't be the same as other in database.

--- Library.py
+++ Library.py
@@ -7,7 +7,7 @@ class Library:
         self.registry = {}
 
 
-    def setup(self, path):
+    def setup(self):
         if os.path.exists("C:/Users/micha/Documents/Python/PytonMentoring/books.txt") and os.path.exists("C:/Users/micha/Documents/Python/PytonMentoring/users.txt"):
             print("Library successful loaded! ")
             self.books = open("books.txt").read().split()
@@ -16,7 +16,10 @@ class Library:
             print(self.users)
 
         else:
-            print("Library doesn't exist! ")
+            print("Missing some database, Creating new one... ")
+            open("books.txt", "a")
+            open("users.txt", "a")
+            self.setup()
 
     # sprawdz czy pliki books.txt i users.txt istnieją w folderze x
     # jesli tak to zczytaj books i users do atrybutów
@@ -43,37 +46,52 @@ class Library:
             print("This book is already borrowed!")
             return False
 
-    def register_user(self, user_name):
-        if user_name not in self.users:
+    def register_user(self, user_name, user_password):
+        if user_name not in self.users and user_password not in self.users:
             self.users.append(user_name)
+            self.users.append(user_password)
             with open("users.txt", "a") as f:
                 f.write(f"{user_name}\n")
+                f.write(f"{user_password}\n")
             print(f"User '{user_name}' successful added to user database")
             return
         else:
-            user_name = input(f"'{user_name}' is taken. Try Again! ")
-            self.register_user(user_name)
+            if user_name in self.users:
+                user_name = input(f"'{user_name}' is taken. Try Again! ")
+                self.register_user(user_name, user_password)
+            elif user_password in self.users:
+                user_password = input(f"'{user_password}' is taken. Try again: ")
+                self.register_user(user_name, user_password)
 
-        # with open user_file:
-        # dopisz nowego goscia
 
-    def add_new_book(self, book_name):
-        if book_name not in self.books and self.registry.values() != book_name:
-            self.books.append(book_name)
-            with open("books.txt", "a") as f:
-                f.write(f"{book_name}\n")
-            print(f"Book '{book_name}' successful added to library! ")
-        else:
-            print(f"This book '{book_name}' is alredy in Library data base")
+    def add_new_book(self):
+        book_name = input("Input book title (CamelCase): ")
+        while book_name.lower() != "x":
+            if book_name not in self.books and self.registry.values() != book_name:
+                self.books.append(book_name)
+                with open("books.txt", "a") as f:
+                    f.write(f"{book_name}\n")
+                print(f"Book '{book_name}' successful added to library! ")
+                self.add_new_book()
+            else:
+                print(f"This book '{book_name}' is alredy in Library data base")
+                self.add_new_book()
+            break
 
-        # with open books_file:
-        # add new line
 
 lib = Library()
 path = "C:/Users/micha/Documents/Python/PytonMentoring"
-lib.setup(path)
-lib.add_new_book("Book1")
-lib.register_user("Michal")
+lib.setup()
+while True:
+    choice = input("r = register / a = add new book / d = deposit / w = withdraw / e = END ")
+    if choice == "a":
+        lib.add_new_book()
+    elif choice == "r":
+        user_name = input("Input username: ")
+        user_password = input("Input password: ")
+        lib.register_user(user_name, user_password)
+    elif choice == "e":
+        break
 
-print(lib.users)
-print(lib.books)
+print(f"Print bazy ksiazek na koniec {lib.books}")
+print(f"Print bazy user'ow na koniec {lib.users}")
