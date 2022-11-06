d7f3ab5c471123d75460070c96d7ed478b752ac9
Michae7o <michalkotlowski7@gmail.com>
2022-11-06 16:48:37 +0100

    1. Now is one file 'database.json' where is storage users, books and registry. 2. creating function 'available_books_to_borrow' -> this function shows what's books can be borrowed for user. 2a. 'borrow' function now should work correct which database file. 3. creating function 'available_books_to_deposit' -> this function shows what's books can be deposit for user. 3a. 'deposit' function now should work correct which database file. 4. added "user_log_in" function that check correct login and password 5. added "login_available" and "password_available" -> functions checking that login or password is available. 6. "register_user" function should now work with new database file. 7. added "can_be_add" function where is checking the book exists in library. 8. "add_new_book" function now should work with new database file. (This function should work only on 'admin' account. 9. modify "while" loop. Now user on the begining chose between login or register.

--- Library.py
+++ Library.py
@@ -3,110 +3,122 @@ import json
 class Library:
 
     def __init__(self):
-        self.books = {"books": {}}
-        self.users = {"users": {}}
-        self.registry = {"registry": {}}
+        self.database = {"books": [], "users": [{"login": "admin", "password": "admin"}], "registry": []}
 
 
     def setup(self):
         cwd = os.getcwd()
-        if os.path.exists(f"{cwd}\\books.json"):
-            with open("books.json", "r") as f:
-                self.books = json.load(f)
+        if os.path.exists(f"{cwd}\\database.json"):
+            with open("database.json", "r") as f:
+                self.database = json.load(f)
         else:
-            with open("books.json", "a") as f:
-                f.write(json.dumps(self.books))
+            with open("database.json", "a") as f:
+                f.write(json.dumps(self.database))
 
-        if os.path.exists(f"{cwd}\\users.json"):
-            with open("users.json", "r") as f:
-                self.users = json.load(f)
-        else:
-            with open("users.json", "a") as f:
-                f.write(json.dumps(self.users))
-
-        if os.path.exists(f"{cwd}\\registry.json"):
-            with open("registry.json", "r") as f:
-                self.registry = json.load(f)
-        else:
-            with open("registry.json", "a") as f:
-                f.write(json.dumps(self.registry))
-        print("Library successful loaded! ")
-        print(self.books)
-        print(self.users)
-        print(self.registry)
-
-    def borrow(self, user, password, book):
-        if user in self.users["users"] and password in self.users["users"][user]:
-            if self.available_books(book):
-                self.books["books"][book].append("BORROWED")
-                with open("books.json", "w") as f:
-                    f.write(json.dumps(self.books))
-                self.registry['registry'].update({user: book})
-                with open("registry.json", "w") as f:
-                    f.write(json.dumps(self.registry))
-                    print(f"{user} just borrow book: {book}")
+    def available_books_to_borrow(self):
+        print("Books available to borrow:")
+        for i in self.database["books"]:
+            if i["borrow"] == "No":
+                print(i["title"])
             else:
-                print(f"{user} do not have book: {book}")
-        else:
-            print(f"Username or password is incorrect, Try again ")
+                pass
 
-    def deposit(self, user, password, book):
-        if user in self.users["users"] and password in self.users["users"][user]:
-            if user in self.registry["registry"] and book in self.registry["registry"][user]:
-                self.books["books"][book].remove("BORROWED")
-                with open("books.json", "w") as f:
-                    f.write(json.dumps(self.books))
-                self.registry['registry'].update({user: ""})
-                with open("registry.json", "w") as f:
-                    f.write(json.dumps(self.registry))
+    def borrow(self, user):
+        self.available_books_to_borrow()
+        book = input("Which book you want to borrow: ")
+        for i in self.database["books"]:
+            if i["title"] == book:
+                i["borrow"] = "Yes"
+                with open("database.json", "w") as f:
+                    f.write(json.dumps(self.database))
+                self.database['registry'].append({'user': user, 'book': book})
+                with open("database.json", "w") as f:
+                    f.write(json.dumps(self.database))
+                print(f"{user} just borrow book: {book}")
+
+    def available_books_to_deposit(self, user, books):
+        print("Books available to deposit:")
+        for i in self.database["registry"]:
+            if i["user"] == user:
+                print(i["book"])
+                books.append(i["book"])
+            else:
+                pass
+
+    def deposit(self, user):
+        books = []
+        self.available_books_to_deposit(user, books)
+        book = input("Which book you want to return: ")
+        if book in books:
+            for i in self.database["registry"]:
+                self.database["registry"].remove(i)
+            for b in self.database["books"]:
+                if b["title"] == book:
+                    b["borrow"] = "No"
+            with open("database.json", "w") as f:
+                f.write(json.dumps(self.database))
                 print(f"{user} just deposit book: {book}")
-            else:
-                print(f"{user} do not have book: {book}")
         else:
-            print(f"Username or password is incorrect, Try again ")
+            print(f"You don't have book: {book}")
+            book = input("Which book you want to return: ")
+            self.deposit(user)
 
-    def available_books(self, book):
-        if book in self.books["books"] and "BORROWED" not in self.books["books"][book]:
-            return True
-        else:
-            print("This book is already borrowed!")
+    def user_log_in(self, login, password):
+        if login != "" and password != "":
+            for i in self.database["users"]:
+                if login == i["login"] and password == i["password"]:
+                    return True
             return False
 
-    def password_available(self,user_password):
-        for i in self.users["users"]:
-            if self.users["users"][i] == user_password:
+    def login_available(self, user_name):
+        for i in self.database["users"]:
+            if i["login"] == user_name:
+                return False
+        return True
+
+    def password_available(self, user_password):
+        for i in self.database["users"]:
+            if i["password"] == user_password:
                 return False
         return True
 
     def register_user(self, user_name, user_password):
-        if user_name not in self.users["users"] and self.password_available(user_password):
-            self.users["users"].update({user_name: user_password})
-            with open("users.json", "w") as f:
-                f.write(json.dumps(self.users))
+        if self.login_available(user_name) and self.password_available(user_password):
+            self.database["users"].append({"login": user_name, "password": user_password})
+            with open("database.json", "w") as f:
+                f.write(json.dumps(self.database))
             print("User successful register! ")
-            print(self.users)
+            print(self.database)
         else:
-            if user_name in self.users["users"]:
-                user_name = input(f"'{user_name}' is taken. Try Again! ")
+            if not self.login_available(user_name):
+                user_name = input(f"'{user_name}' is taken. Try again: ")
                 self.register_user(user_name, user_password)
             elif not self.password_available(user_password):
                 user_password = input(f"'{user_password}' is taken. Try again: ")
+
                 self.register_user(user_name, user_password)
 
+    def can_be_add(self, book):
+        for i in self.database["books"]:
+            if i["title"] == book:
+                return False
+        return True
 
     def add_new_book(self):
-        book_name = input("Input book title (CamelCase): -to close input 'x'  ")
+        book_name = input("Input book title: -to close input 'x' ")
         while book_name.lower() != "x":
-            if book_name not in self.books["books"]:
+            if self.can_be_add(book_name):
                 book_author = input("Input book Author: ")
                 book_pages = input("Input book pages: ")
-                self.books['books'].update({book_name: [book_author, book_pages]})
-                with open("books.json", "w") as f:
-                    f.write(json.dumps(self.books))
+                self.database['books'].append(
+                    {"title": book_name, "author": book_author, "pages": book_pages, "borrow": "No"})
+                print(self.database)
+                with open("database.json", "w") as f:
+                    f.write(json.dumps(self.database))
                 print(f"Title:'{book_name}', Author:'{book_author}', pages:'{book_pages}' - successful added to Library")
                 self.add_new_book()
             else:
-                print(f"This book '{book_name}' is alredy in Library or have invalid title")
+                print(f"This book '{book_name}' is already in Library or have invalid title")
                 self.add_new_book()
             break
 
@@ -114,21 +126,27 @@ class Library:
 lib = Library()
 lib.setup()
 while True:
-    choice = input("r = register / a = add new book / d = deposit / b = borrow / e = END ")
-    if choice == "a":
-        lib.add_new_book()
-    elif choice == "r":
+    log_in = input("Login = l / Register = r ").lower()
+    if log_in == "l":
+        login = input("Input username: ")
+        password = input("Input user password: ")
+        if lib.user_log_in(login, password):
+            while True:
+                choice = input(" a = add new book (only admin) / d = deposit / b = borrow / e = Logout ")
+                if choice == "a" and login == "admin" and password == "admin":
+                    lib.add_new_book()
+                elif choice == "a" and login != "admin" and password != "admin":
+                    print("This function is only for Administrator! ")
+                elif choice == "b":
+                    lib.borrow(login)
+                elif choice == "d":
+                    lib.deposit(login)
+                elif choice == "e":
+                    break
+    elif log_in == "r":
         user_name = input("Input username: ")
         user_password = input("Input password: ")
-        lib.register_user(user_name, user_password)
-    elif choice == "b":
-        lib.borrow(user=input("Input username: "),password=input("Input user password: "), book=input("Input book title to borrow: "))
-    elif choice == "d":
-        lib.deposit(user=input("Input username: "), password=input("Input user password: "),
-                   book=input("Input book title to deposit: "))
-    elif choice == "e":
-        break
-
-print(f"Print bazy ksiazek na koniec {lib.books}")
-print(f"Print bazy user'ow na koniec {lib.users}")
-print(f"Print rejestru na koniec {lib.registry}")
+        if user_name.isalnum() and user_password.isalnum():
+            lib.register_user(user_name, user_password)
+        else:
+            print("Username or password is invalid!")
